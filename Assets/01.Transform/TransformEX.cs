ec17f45555902211193867380e02e0d1c85e73a9
Dydals <dydals4289@gmail.com>
2025-04-23 18:26:43 +0900

    0423
    Component_Text
    Transform_Text
    Renderer_Text
    Collider_Text
    Rigidbody_Text

---
+++ Assets/01.Transform/TransformEX.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TransformEX : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/04.Rigidbody/RigidbodyExample.cs
@@ -0,0 +1,17 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RigidbodyExample : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
